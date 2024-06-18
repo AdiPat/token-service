102990b9c673d2387ee1865c5fc29ae0a1da8244
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-18 19:58:28 +0530

    add token info endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getToken(tokenId: string): Promise<any> {
+    return {
+      // @ts-ignore
+      tokenId,
+      bitcoin: {
+        usd: 64835,
+        usd_market_cap: 1277989080236.3804,
+        usd_24h_vol: 38146650224.13893,
+        usd_24h_change: -0.3872257517061325,
+        last_updated_at: 1718719199,
+      },
+    };
   }
 }
