fb0e032897c1131d51a467154f3e7c95b389f31d
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-19 21:13:23 +0530

    add interval to reset token buckets

--- src/common/access-key-cache.ts
+++ src/common/access-key-cache.ts
@@ -1,4 +1,4 @@
-import { AccessKeyDetails } from '../models';
+import { AccessKeyDetails, RunningRateLimit } from '../models';
 import { redis } from '.';
 
 class AccessKeyCache_ {
@@ -7,7 +7,7 @@ class AccessKeyCache_ {
   ): Promise<boolean> {
     try {
       const json = JSON.stringify(accessKeyDetails);
-      await redis.set(accessKeyDetails.accessKey, json);
+      await redis.set(`accessKeyDetails:${accessKeyDetails.accessKey}`, json);
       return true;
     } catch (err) {
       console.error('Failed to set access key details', err);
@@ -15,14 +15,34 @@ class AccessKeyCache_ {
     }
   }
 
-  async getAccessKeyDetails(accessKey: string): Promise<AccessKeyDetails> {
+  async getAccessKeyDetails(accessKeyValue: string): Promise<AccessKeyDetails> {
     try {
-      const json = await redis.get(accessKey);
+      const json = await redis.get(`accessKeyDetails:${accessKeyValue}`);
       return JSON.parse(json);
     } catch (err) {
       console.error('Failed to get access key details', err);
       return null;
     }
+  }
+
+  async resetRateLimitBuckets(): Promise<void> {
+    setInterval(async () => {
+      const keys = await redis.keys('rateLimit:*');
+      keys.map(async (key) => {
+        const runningRateLimitsJson = await redis.get(key);
+        const runningRateLimits: RunningRateLimit = JSON.parse(
+          runningRateLimitsJson,
+        );
+        const accessKeyDetailsJson = await redis.get(
+          `accessKeyDetails:${runningRateLimits.accessKeyValue}`,
+        );
+        const accessKeyDetails = JSON.parse(
+          accessKeyDetailsJson,
+        ) as AccessKeyDetails;
+        runningRateLimits.remainingLimit = accessKeyDetails.limitPerSecond;
+        redis.set(key, JSON.stringify(runningRateLimits));
+      });
+    }, 60 * 1000); // hardcoded to update every minute for now
   }
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AccessKeyCache } from './common/access-key-cache';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  AccessKeyCache.resetRateLimitBuckets();
   await app.listen(3000);
 }
 bootstrap();
