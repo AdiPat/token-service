8acb72581976b3e4c767d8e3e5a773ac40b8e104
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-20 05:48:31 +0530

    tests/e2e: add e2e tests for rate limiter

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/core';
+import { redis } from '../src/common';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -13,12 +14,129 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    await redis.flushdb();
   });
 
-  it('/ (GET)', () => {
+  describe('/tokens/{tokenId} (GET)', () => {
+    it('/tokens/bitcoin (GET): fails with UNAUTHORIZED', () => {
+      return request(app.getHttpServer())
+        .get('/tokens/bitcoin')
+        .expect(HttpStatus.UNAUTHORIZED);
+    });
+
+    it('/tokens/bitcoin (GET): valid key allows request', async () => {
+      const xApiKey = 'validkey';
+      await redis.set(
+        `accessKeyDetails:${xApiKey}`,
+        JSON.stringify({
+          accessKey: xApiKey,
+          limitPerSecond: 100,
+          disabled: false,
+          expiry: new Date().getTime() + 1000 * 60 * 60 * 24,
+        }),
+      );
+
+      return request(app.getHttpServer())
+        .get('/tokens/bitcoin')
+        .expect(HttpStatus.OK)
+        .set('x-api-key', xApiKey)
+        .expect((res) => {
+          if (!res.body) {
+            throw new Error('Invalid response body');
+          }
+        });
+    });
+
+    it('/tokens/bitcoin (GET): disabled key fails with UNAUTHORIZED', async () => {
+      const xApiKey = 'disabledkey';
+      await redis.set(
+        `accessKeyDetails:${xApiKey}`,
+        JSON.stringify({
+          accessKey: xApiKey,
+          limitPerSecond: 100,
+          disabled: true,
+          expiry: new Date().getTime() + 1000 * 60 * 60 * 24,
+        }),
+      );
+
+      return request(app.getHttpServer())
+        .get('/tokens/bitcoin')
+        .expect(HttpStatus.UNAUTHORIZED)
+        .set('x-api-key', xApiKey);
+    });
+
+    it('/tokens/bitcoin (GET): expired key fails with UNAUTHORIZED', async () => {
+      const xApiKey = 'expiredkey';
+      await redis.set(
+        `accessKeyDetails:${xApiKey}`,
+        JSON.stringify({
+          accessKey: xApiKey,
+          limitPerSecond: 100,
+          disabled: false,
+          expiry: new Date().getTime() - 1000 * 60 * 60 * 24,
+        }),
+      );
+
+      return request(app.getHttpServer())
+        .get('/tokens/bitcoin')
+        .expect(HttpStatus.UNAUTHORIZED)
+        .set('x-api-key', xApiKey);
+    });
+
+    it('/tokens/bitcoin (GET): rate limited key fails with TOO_MANY_REQUESTS', async () => {
+      const xApiKey = 'ratelimitedkey';
+      await redis.set(
+        `accessKeyDetails:${xApiKey}`,
+        JSON.stringify({
+          accessKey: xApiKey,
+          limitPerSecond: 1,
+          disabled: false,
+          expiry: new Date().getTime() + 1000 * 60 * 60 * 24,
+        }),
+      );
+
+      await redis.set(
+        `rateLimit:${xApiKey}`,
+        JSON.stringify({
+          accessKeyValue: xApiKey,
+          interval: 60,
+          remainingLimit: 0,
+          lastReset: new Date(),
+        }),
+      );
+
+      return request(app.getHttpServer())
+        .get('/tokens/bitcoin')
+        .expect(HttpStatus.TOO_MANY_REQUESTS)
+        .set('x-api-key', xApiKey);
+    });
+  });
+
+  it('/tokens/bitcoin (GET): rate limited key fails with TOO_MANY_REQUESTS for multiple requests', async () => {
+    const requestLimit = 10;
+
+    await redis.flushdb();
+    const xApiKey = 'ratelimitedkey';
+    await redis.set(
+      `accessKeyDetails:${xApiKey}`,
+      JSON.stringify({
+        accessKey: xApiKey,
+        limitPerSecond: requestLimit, // this should be limit per min
+        disabled: false,
+        expiry: new Date().getTime() + 1000 * 60 * 60 * 24,
+      }),
+    );
+
+    for (let i = 0; i < requestLimit; i++) {
+      await request(app.getHttpServer())
+        .get('/tokens/bitcoin')
+        .expect(HttpStatus.OK)
+        .set('x-api-key', xApiKey);
+    }
+
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get('/tokens/bitcoin')
+      .expect(HttpStatus.TOO_MANY_REQUESTS)
+      .set('x-api-key', xApiKey);
   });
 });
